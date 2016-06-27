76bd2c9809a19f275dd72580b2fb947b0cc3c59d
tablesmit <tablesmit@gmail.com>
2016-06-27 21:24:06 +0700

    Create MousePostion.js

---
+++ MousePostion.js
@@ -0,0 +1,11 @@
+function myFunction(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("demo").innerHTML = coor;
+
+}
+
+function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+}
